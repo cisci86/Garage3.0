579d9ef1cb3af6714cf4d83df0004ce72ca1195a
Cecilia Sjögren <cisci_13@hotmail.com>
2022-01-31 16:06:52 +0100

    Removed some comments and removed the arrival part of the edit form

--- Garage 2.0/Models/Vehicle.cs
+++ Garage 2.0/Models/Vehicle.cs
@@ -21,13 +21,6 @@ namespace Garage_2._0.Models
         public int Wheels { get; set; }
         [ReadOnly(true)] //Not sure if this is needed
         public DateTime Arrival { get; set; }
-        //The error message saids that we need to have an empty constructor so I removed the one we hade.
 
-        // I tried this but then all of the times in all the items changed to the time that I added the last thing. I moved It to the Control in the Create method.
-
-        //public Vehicle() 
-        //{
-        //    Arrival = DateTime.Now;
-        //}
     }
 }
