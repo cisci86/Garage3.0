8612786c4d4a1c45c1625847518b6a086d219f7a
Cecilia Sjögren <cisci_13@hotmail.com>
2022-02-15 15:47:46 +0100

    Added Member Entity with controller and DB. Added Member prop in Vehicle

--- Garage 2.0/Data/GarageVehicleContext.cs
+++ Garage 2.0/Data/GarageVehicleContext.cs
@@ -27,6 +27,22 @@ using Garage_2._0.Models;
                new Vehicle { Type = Garage_2._0.Interfaces.VehicleTypes.Motorcycle, License = "MXP123", Color = "Yellow", Make = "Volvo", Model = "Xc60", Wheels = 2, Arrival = DateTime.Parse("2022 - 02 - 01 14:09:28"), ParkingSpot = 3 },
                new Vehicle { Type = Garage_2._0.Interfaces.VehicleTypes.Bus, License = "RRH123", Color = "Blue", Make = "Volvo", Model = "Xc60", Wheels = 8, Arrival = DateTime.Parse("2022 - 02 - 01 15:09:28"), ParkingSpot = 4 }
             );
+
+        modelBuilder.Entity<Member>()
+                    .OwnsOne(m => m.Name)
+                    .Property(n => n.FirstName)
+                    .HasColumnName("FirstName");
+        modelBuilder.Entity<Member>()
+                    .OwnsOne(m => m.Name)
+                    .Property(n => n.LastName)
+                    .HasColumnName("LastName");
+
+        modelBuilder.Entity<Member>()
+                    .HasMany(m => m.Vehicles);
     }
+
+    //adds seed data to the database
+    public DbSet<Garage_2._0.Models.Member> Member { get; set; }
+
 
 }
