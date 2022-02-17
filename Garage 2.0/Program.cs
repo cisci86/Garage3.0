105fa7fbd10358b1bcff2c7797e7f280bc72b501
Radha Manickam <radhaarul@gmail.com>
2022-02-17 08:55:03 +0100

    update asp-action at layout for memberoverview

--- Garage 2.0/Program.cs
+++ Garage 2.0/Program.cs
@@ -30,6 +30,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Members}/{action=MemberOverViewIndex}/{id?}");
+    pattern: "{controller=Vehicles}/{action=VehiclesOverview}/{id?}");
 
 app.Run();
