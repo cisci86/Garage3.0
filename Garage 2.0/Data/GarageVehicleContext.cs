5fda0bdbba27270ba7f77c3d7250124055bdcbfd
Cecilia Sjögren <cisci_13@hotmail.com>
2022-02-18 14:11:30 +0100

    Removed seed from Context file

--- Garage 2.0/Data/GarageVehicleContext.cs
+++ Garage 2.0/Data/GarageVehicleContext.cs
@@ -20,14 +20,6 @@ public class GarageVehicleContext : DbContext
     {
         base.OnModelCreating(modelBuilder);
 
-        modelBuilder.Entity<Vehicle>()
-            .HasData(
-               new Vehicle { Type = Garage_2._0.Interfaces.VehicleTypes.Car, License = "EGW123", Color="Red", Make="Volvo", Model="Xc60",Wheels=4,Arrival= DateTime.Parse("2022-02-01 12:09:28"), ParkingSpot = 1 },
-               new Vehicle { Type = Garage_2._0.Interfaces.VehicleTypes.Car, License = "ASL123", Color = "White", Make = "Volvo", Model = "Xc60", Wheels = 4, Arrival = DateTime.Parse("2022 - 02 - 01 13:09:28"), ParkingSpot = 2},
-               new Vehicle { Type = Garage_2._0.Interfaces.VehicleTypes.Motorcycle, License = "MXP123", Color = "Yellow", Make = "Volvo", Model = "Xc60", Wheels = 2, Arrival = DateTime.Parse("2022 - 02 - 01 14:09:28"), ParkingSpot = 3 },
-               new Vehicle { Type = Garage_2._0.Interfaces.VehicleTypes.Bus, License = "RRH123", Color = "Blue", Make = "Volvo", Model = "Xc60", Wheels = 8, Arrival = DateTime.Parse("2022 - 02 - 01 15:09:28"), ParkingSpot = 4 }
-            );
-
         modelBuilder.Entity<Member>()
                     .OwnsOne(m => m.Name)
                     .Property(n => n.FirstName)
